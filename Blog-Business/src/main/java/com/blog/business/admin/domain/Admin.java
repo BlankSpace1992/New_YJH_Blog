2a184982bc3c7ba991b21385c44e50bb22997ea9
YuJunHong <yu459318144@outlook.com>
2021-10-21 11:29:58 +0800

    1 设置uid以及自动更新时间

--- Blog-Business/src/main/java/com/blog/business/admin/domain/Admin.java
+++ Blog-Business/src/main/java/com/blog/business/admin/domain/Admin.java
@@ -1,10 +1,7 @@
 package com.blog.business.admin.domain;
 
 import cn.afterturn.easypoi.excel.annotation.Excel;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import com.blog.business.web.domain.Role;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -34,7 +31,7 @@ public class Admin {
      */
     @ApiModelProperty(value = "唯一uid")
     @Excel(name = "唯一uid")
-    @TableId(value = "uid", type = IdType.AUTO)
+    @TableId(value = "uid", type = IdType.ASSIGN_UUID)
     private String uid;
     /**
      * 用户名
@@ -113,12 +110,14 @@ public class Admin {
      */
     @ApiModelProperty(value = "创建时间")
     @Excel(name = "创建时间")
+    @TableField(value = "create_time",fill = FieldFill.INSERT)
     private Date createTime;
     /**
      * 更新时间
      */
     @ApiModelProperty(value = "更新时间")
     @Excel(name = "更新时间")
+    @TableField(value = "update_time",fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
     /**
      * 昵称
