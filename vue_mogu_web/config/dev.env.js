810e9140f9e86b18e8675e7734dfe818d174097d
YuJunHong <yu459318144@outlook.com>
2021-11-01 16:44:40 +0800

    1 新增ElasticSearch查询配置文件

--- vue_mogu_web/config/dev.env.js
+++ vue_mogu_web/config/dev.env.js
@@ -8,6 +8,6 @@ module.exports = merge(prodEnv, {
   VUE_MOGU_WEB: '"http://localhost:9527"',
   PICTURE_API: '"http://localhost:8089/cloud-blog-picture"',
   WEB_API: '"http://localhost:8089/cloud-blog-web"',
-  SEARCH_API: '"http://localhost:8607/mogu-search"',
+  SEARCH_API: '"http://localhost:8089/cloud-blog-search"',
 
 })
